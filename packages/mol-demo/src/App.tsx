e66a5b21bf8000ada71628b6c07640c5ca70d556
joska <jpotin@gmail.com>
2026-09-20 15:20:39 +0200

    100%h

--- packages/mol-demo/src/App.tsx
+++ packages/mol-demo/src/App.tsx
@@ -33,7 +33,7 @@ export function App() {
         }
       >
         <Stage label="mol-demo">
-          <div style={{ width: "100%", height: 420 }}>
+          <div style={{ width: "100%", height: "100%" }}>
             <MolCanvas />
           </div>
         </Stage>
