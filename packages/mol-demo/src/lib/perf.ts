fbee644b83d716ce4618c20614159468ae6754e4
joska <jpotin@gmail.com>
2026-09-23 11:41:42 +0200

    Update perf.ts

--- packages/mol-demo/src/lib/perf.ts
+++ packages/mol-demo/src/lib/perf.ts
@@ -44,9 +44,8 @@ export function isPerfEnabled(): boolean {
         return false;
     }
     try {
-        return (
-            import.meta.env.DEV === true && new URLSearchParams(window.location.search).has('perf')
-        );
+        return true; // entry point to disable the perf
+        // import.meta.env.DEV === true && new URLSearchParams(window.location.search).has('perf')
     } catch {
         return false;
     }
